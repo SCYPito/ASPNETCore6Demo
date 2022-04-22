c8c904b21d1de46e66fd26fd6980c7c6cdc7c13f
SCYPito <88193725+SCYPito@users.noreply.github.com>
2022-04-22 20:31:08 +0800

    Blazor前端框架

--- ASPNETCore6Demo/ASPNETCore6/Program.cs
+++ ASPNETCore6Demo/ASPNETCore6/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using System;
+
+//namespace ASPNETCore6
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+//在ConsoleApp下可將 using namespace class 省略，在combine的過程中編譯器自動補上using namespace class
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, "+args[0]+"!");
+
+
+/*
+ <Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net6.0</TargetFramework>
+      //目標框架
+    <ImplicitUsings>enable</ImplicitUsings>
+      //隱含的Using 在Microsoft.NET.Sdk下
+      //******補充global using的用法
+    <Nullable>enable</Nullable>
+  </PropertyGroup>
+
+</Project>
+ */
